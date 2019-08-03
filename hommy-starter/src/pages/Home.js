4dc36c024875df236654fda4c208452b46ad5664
Ana Nikolova <ani.nik19@gmail.com>
2019-08-03 10:00:02 +0200

    Services Section done

--- hommy-starter/src/pages/Home.js
+++ hommy-starter/src/pages/Home.js
@@ -2,19 +2,23 @@ import React from "react";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
+import Services from "../components/Services";
 
 const Home = () => {
   return (
-    <Hero>
-      <Banner
-        title="Discover a place you'll love to live"
-        subtitle="Don't settle for average"
-      >
-        <Link to="/houses" className="btn-primary">
-          Your new home
-        </Link>
-      </Banner>
-    </Hero>
+    <>
+      <Hero>
+        <Banner
+          title="Discover a place you'll love to live"
+          subtitle="Don't settle for average"
+        >
+          <Link to="/houses" className="btn-primary">
+            Your new home
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+    </>
   );
 };
 
