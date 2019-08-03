e7aa50fa5574e89201f60492d414b0fa3373f557
Ana Nikolova <ani.nik19@gmail.com>
2019-08-03 07:59:56 +0200

    Banner done

--- hommy-starter/src/pages/Home.js
+++ hommy-starter/src/pages/Home.js
@@ -1,8 +1,21 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <Hero />;
+  return (
+    <Hero>
+      <Banner
+        title="Discover a place you'll love to live"
+        subtitle="Don't settle for average"
+      >
+        <Link to="/houses" className="btn-primary">
+          Your new home
+        </Link>
+      </Banner>
+    </Hero>
+  );
 };
 
 export default Home;
--- hommy-starter/src/pages/Houses.js
+++ hommy-starter/src/pages/Houses.js
@@ -1,8 +1,18 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 const Houses = () => {
-  return <Hero hero="housesHero" />;
+  return (
+    <Hero hero="housesHero">
+      <Banner title="our houses">
+        <Link to="/" className="btn-primary">
+          return home
+        </Link>
+      </Banner>
+    </Hero>
+  );
 };
 
 export default Houses;
