55b359f6dbfe623f43e149aa55b2a6ef4356d8dd
Ana Nikolova <ani.nik19@gmail.com>
2019-08-03 06:38:10 +0200

    navbar done

--- hommy-starter/src/App.js
+++ hommy-starter/src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
-//import "./App.css";
+import "./App.css";
 import Home from "./pages/Home";
 import Houses from "./pages/Houses";
 import SingleHouse from "./pages/SingleHouse";
 import Error from "./pages/Error";
 import { Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/house" component={Houses} />
+        <Route exact path="/houses" component={Houses} />
         <Route exact path="/single-house/:slug" component={SingleHouse} />
         <Route component={Error} />
       </Switch>
--- hommy-starter/src/pages/Home.js
+++ hommy-starter/src/pages/Home.js
@@ -8,3 +8,4 @@ export default Home;
 
 //rafc
 //rfc
+//rcc
